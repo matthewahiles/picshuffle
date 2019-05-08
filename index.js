2325be9086be777dbc17c0eef9a726b2ca821ada
matthewahiles <matthewahiles@gmail.com>
2019-05-08 18:14:29 -0500

    quick change to eagerly load albums

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const {getRandomImage, postAlbum} = require('./lib/routes/imgur-routes')
+const {getRandomImage} = require('./lib/routes/imgur-routes')
 
 const app = express()
 
 // Redirect to sfw album hardcoded for now
 app.get('/', (req, res) => res.redirect('/vgW1p'))
 app.get('/:albumId', getRandomImage)
-app.post('/:albumId', postAlbum)
 
 app.listen(3000, () => console.log('Server listening on 3000'))
